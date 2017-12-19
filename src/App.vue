fbcc644d4d074cc0ae824e5746bdda623d6700e8
hzCat <haizhoucat@qq.com>
2017-12-19 17:17:20 +0800

    vue-webpack

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <div class="tabbar">
+      <div class="one">首页</div>
+      <div class="one">个人中心</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "app"
+  };
+
+</script>
+
+<style>
+  #app {
+    margin:0;
+    padding:0;
+    margin-bottom:0.96rem
+  }
+
+</style>
