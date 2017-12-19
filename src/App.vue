e15addd325f0516dfd13c1d52b6cbe752fb0085f
hzCat <haizhoucat@qq.com>
2017-12-19 18:29:15 +0800

    img

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
     <div class="tabbar">
-      <div class="one">首页</div>
-      <div class="one">个人中心</div>
+      <div class="one" @click="first">
+        <img :src="path1" alt="">
+        <span>首页</span>
+      </div>
+      <div class="one" @click="center">
+        <img :src="path2" alt="">
+        <span>
+          个人中心
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "app"
-  };
+export default {
+  name: "app",
 
+  data: function() {
+    return {
+      path1: "./../static/tabbar/icon_home_n.png",
+      path2: "./../static/tabbar/icon_persionalCerter.png"
+    };
+  },
+  methods: {
+    first() {
+      if(this.path1=="./../static/tabbar/icon_home_n.png"){
+        this.path1="././../static/tabbar/icon_home_selected.png"
+        this.path2="./../static/tabbar/icon_persionalCerter.png"
+      }
+    },
+    center() {
+      if(this.path2=="./../static/tabbar/icon_persionalCerter.png"){
+        this.path2="././../static/tabbar/icon_persionalCerter-selected.png"
+        this.path1="./../static/tabbar/icon_home_n.png"
+      }
+    }
+  }
+};
 </script>
 
 <style>
-  #app {
-    margin:0;
-    padding:0;
-    margin-bottom:0.96rem
-  }
-
+#app {
+  margin: 0;
+  padding: 0;
+  margin-bottom: 0.96rem;
+}
 </style>
