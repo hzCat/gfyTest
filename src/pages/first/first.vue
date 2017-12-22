dd341a0d107774c7371392e7c9a86d8c1e485120
hzCat <haizhoucat@qq.com>
2017-12-22 13:51:50 +0800

    first-page html-done

--- src/pages/first/first.vue
+++ src/pages/first/first.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="container">
-      <div class="bg-img"></div>
-      <div class="cover">
-          <div class="circle"></div>
-          <div class="circle">
-              <img src="../../assets/img/index/logo.png" alt="">
-          </div>
-          <div class="circle"></div>
+    <div class="bg-img"></div>
+    <div class="cover">
+      <div class="circle"></div>
+      <div class="circle">
+        <img src="../../assets/img/index/logo.png" alt="">
       </div>
-      <div class="bottom-bg">
-          <div class="scan"></div>
+      <div class="circle"></div>
+    </div>
+    <div class="bottom-bg">
+      <div class="scan">
+        <div class="title">
+          <img src="../../assets/img/index/icon_scan.png">
+        </div>
+        <div class="content">扫码</div>
       </div>
+    </div>
   </div>
 </template>
+
 <script>
-import "./style.css";
-export default {
-  data: function() {
-    return {};
-  }
-};
+  import "./style.css";
+  export default {
+    data: function () {
+      return {};
+    }
+  };
+
 </script>
